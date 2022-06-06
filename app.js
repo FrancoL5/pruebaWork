35ccf8636033a9abacdd0e4d436ad9bc2ee8f947
Franco Luna <francovalentin5005@gmail.com>
2022-06-06 15:45:26 -0300

    helper

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const dirFile = process.argv[3];
 
 
 switch (command){
+    case "help":
+        console.log(`comandos y ejemplos: 
+        =>HsExtra [nombreArchivo.xlsx]
+        | EJ: node app.js HsExtra miArchivo.xlsx
+        | Importante: el archivo tiene que estar en data y va a ser modificado, se recomienda dejar una copia en dataBackUp 
+        | comment: evitar espacios en el nombre del archivo`);
+        break;
     case "HsExtra":
         horasExtras(dirFile);
         break;
